08c4d48bdcf6c47a213fc82317c952502628f2b4
GlennFolker <glenn.folk08@gmail.com>
2023-03-25 15:32:22 +0700

    Keep updating even if cursor is out

--- src/cix/eye.rs
+++ src/cix/eye.rs
@@ -18,6 +18,7 @@ pub fn cix_update_eye_sys(
     camera: Query<(&Camera, &GlobalTransform)>,
     cix: Query<&CixDirection>,
     mut eye: Query<(&mut Transform, &GlobalTransform), With<CixEye>>,
+    mut cursor_pos: Local<Vec2>,
 ) {
     let &dir = cix.single();
 
@@ -29,36 +30,38 @@ pub fn cix_update_eye_sys(
         .physical_cursor_position()
         .and_then(|pos| camera.viewport_to_world_2d(camera_trns, pos))
     {
-        let eye_trns = global_trns.translation();
-
-        let vec = (pos - Vec2::new(eye_trns.x, eye_trns.y)).clamp_length_max(CixEye::FOCUS);
-        let mut len = vec.length() / CixEye::FOCUS;
-        len = 1. - (len - 1.) * (len - 1.);
-        len *= CixEye::DEVIATE;
-
-        let mut prog = dir.progress;
-        prog = prog * prog * (3. - 2. * prog);
-        prog = (prog * 2. - 1.) * if dir.right { 1. } else { -1. };
-
-        let mut deviate = vec * ((len * len) / vec.length_squared()).sqrt();
-        deviate = Vec2::new(
-            deviate.x * CixEye::COVERAGE + prog * CixEye::DEVIATE * (1. - CixEye::COVERAGE),
-            deviate.y,
-        );
-
-        let tilt = vec / CixEye::FOCUS;
-        let mut tilt_left =
-            tilt.dot(Vec2::from_angle(45f32.to_radians())).max(0.) +
-            tilt.dot(Vec2::from_angle(225f32.to_radians())).max(0.);
-        tilt_left = (1. - (tilt_left - 1.) * (tilt_left - 1.)) * CixEye::TILT;
-
-        let mut tilt_right =
-            tilt.dot(Vec2::from_angle(135f32.to_radians())).max(0.) +
-            tilt.dot(Vec2::from_angle(315f32.to_radians())).max(0.);
-        tilt_right = (1. - (tilt_right - 1.) * (tilt_right - 1.)) * -CixEye::TILT;
-
-        trns.translation.x = deviate.x;
-        trns.translation.y = deviate.y;
-        trns.rotation = Quat::from_axis_angle(Vec3::Z, tilt_left + tilt_right);
+        *cursor_pos = pos;
     }
+
+    let eye_trns = global_trns.translation();
+
+    let vec = (*cursor_pos - Vec2::new(eye_trns.x, eye_trns.y)).clamp_length_max(CixEye::FOCUS);
+    let mut len = vec.length() / CixEye::FOCUS;
+    len = 1. - (len - 1.) * (len - 1.);
+    len *= CixEye::DEVIATE;
+
+    let mut prog = dir.progress;
+    prog = prog * prog * (3. - 2. * prog);
+    prog = (prog * 2. - 1.) * if dir.right { 1. } else { -1. };
+
+    let mut deviate = vec * ((len * len) / vec.length_squared()).sqrt();
+    deviate = Vec2::new(
+        deviate.x * CixEye::COVERAGE + prog * CixEye::DEVIATE * (1. - CixEye::COVERAGE),
+        deviate.y,
+    );
+
+    let tilt = vec / CixEye::FOCUS;
+    let mut tilt_left =
+        tilt.dot(Vec2::from_angle(45f32.to_radians())).max(0.) +
+        tilt.dot(Vec2::from_angle(225f32.to_radians())).max(0.);
+    tilt_left = (1. - (tilt_left - 1.) * (tilt_left - 1.)) * CixEye::TILT;
+
+    let mut tilt_right =
+        tilt.dot(Vec2::from_angle(135f32.to_radians())).max(0.) +
+        tilt.dot(Vec2::from_angle(315f32.to_radians())).max(0.);
+    tilt_right = (1. - (tilt_right - 1.) * (tilt_right - 1.)) * -CixEye::TILT;
+
+    trns.translation.x = deviate.x;
+    trns.translation.y = deviate.y;
+    trns.rotation = Quat::from_axis_angle(Vec3::Z, tilt_left + tilt_right);
 }
