30287637281241aeb6a68c6c87e5a3cfc48d7534
GlennFolker <glenn.folk08@gmail.com>
2023-03-25 15:05:16 +0700

    Eye positioning

--- src/cix/eye.rs
+++ src/cix/eye.rs
@@ -1,19 +1,26 @@
 use bevy::prelude::*;
 
+use crate::CixDirection;
+
 #[derive(Component)]
 pub struct CixEye;
 impl CixEye {
     pub const COLOR: Color = Color::rgb(1., 2.4, 4.8);
-    pub const DEVIATE: f32 = 8.;
-    pub const TILT: f32 = 0.2;
+    pub const TILT: f32 = 0.17;
     pub const FOCUS: f32 = 480.;
+
+    pub const DEVIATE: f32 = 8.5;
+    pub const COVERAGE: f32 = 0.4;
 }
 
 pub fn cix_update_eye_sys(
     window: Query<&Window>,
     camera: Query<(&Camera, &GlobalTransform)>,
+    cix: Query<&CixDirection>,
     mut eye: Query<(&mut Transform, &GlobalTransform), With<CixEye>>,
 ) {
+    let &dir = cix.single();
+
     let Ok(window) = window.get_single() else { return };
     let (camera, camera_trns) = camera.single();
     let (mut trns, &global_trns) = eye.single_mut();
@@ -29,7 +36,15 @@ pub fn cix_update_eye_sys(
         len = 1. - (len - 1.) * (len - 1.);
         len *= CixEye::DEVIATE;
 
-        let deviate = vec * ((len * len) / vec.length_squared()).sqrt();
+        let mut prog = dir.progress;
+        prog = prog * prog * (3. - 2. * prog);
+        prog = (prog * 2. - 1.) * if dir.right { 1. } else { -1. };
+
+        let mut deviate = vec * ((len * len) / vec.length_squared()).sqrt();
+        deviate = Vec2::new(
+            deviate.x * CixEye::COVERAGE + prog * CixEye::DEVIATE * (1. - CixEye::COVERAGE),
+            deviate.y,
+        );
 
         let tilt = vec / CixEye::FOCUS;
         let mut tilt_left =
